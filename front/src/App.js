b29a7a0047c1c442dce53a45390f73a76121ff70
Wilson Arrubla Hoyos <wahs_30@hotmail.com>
2022-10-21 08:01:30 -0500

    Clase 6 Creación de carpeta front end instalación de react y recursos

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+  <div className="App">
+    <h1>Tienda de mascotas Vety Shop</h1>
+    <img src="./images/logoWA" alt="Aqui va el logo"></img>
+  </div>
+  );
+}
+
+export default App;
