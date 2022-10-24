baca622df4c215412ddc0717e558a82e54bec0f1
Wilson Arrubla Hoyos <wahs_30@hotmail.com>
2022-10-24 06:35:17 -0500

    Clase 7 se trabaja el Header y footer modo dummie

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import React from 'react';
+import Header from './components/Header';
+import { Footer } from './components/layout/Footer';
 
 function App() {
   return (
   <div className="App">
-    <h1>Tienda de mascotas Vety Shop</h1>
-    <img src="./images/logoWA" alt="Aqui va el logo"></img>
+    <Header />
+< center>contenido aqui </center>
+< center>contenido aqui </center>
+< center>contenido aqui </center>
+< center>contenido aqui </center>
+< center>contenido aqui </center>
+<Footer />
+
   </div>
   );
 }
---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="py-1">
+    <p className="text-center mt-1">
+        WA Galeria de Arte - 2022 -  - Todos los derechos reservados
+    </p>
+    </footer>
+    )
+}
